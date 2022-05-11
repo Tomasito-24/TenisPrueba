12255d7af7e9e6a2a29d49654c5ff8c389e72523
tomas <tomasjromero01@gmail.com>
2022-05-11 13:17:34 +0200

    commit principal

---
+++ src/Persona.java
@@ -0,0 +1,33 @@
+
+public class Persona {
+
+	
+	private String nombre;
+	private int edad;
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void seetNombre(String nombre) {
+		this.nombre= nombre;
+	}
+	public int edad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
